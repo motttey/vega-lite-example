991f0b86cf9380672101ab3adc72e50c1b47ad5e
motttey <motitago@gmail.com>
2022-05-14 21:05:41 +0900

    draw Bar Chart from Child component

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,10 @@
 import embed from 'vega-embed';
-
-const spec = "https://raw.githubusercontent.com/vega/vega/master/docs/examples/bar-chart.vg.json";
-
+import BarChart from '../components/BarChart.js'
 
 function HomePage() {
-  embed('#vis', spec).then((result) => {
-    console.log(result);
-  }).catch((e) => {
-    console.log(e);
-  });
-
   return (
     <div>
-      <div id="vis" />
+      <BarChart />
     </div>
   )
 }
