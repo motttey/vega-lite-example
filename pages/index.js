3664d4d8006e792e5a6db0c287e609cd0441a555
motttey <motitago@gmail.com>
2022-08-27 20:09:00 +0900

    add: layerd chart to index

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import BarChart from '../components/BarChart.js';
-import ConnectedScatterPlot from '../components/ConnectedScatterPlot.js';
 import HorizonChart from '../components/HorizonChart.js';
 import React from 'react';
 import QuantileDotPlot from '../components/QuantileDotPlot.js';
+import LayerdChart from '../components/LayerdChart.js';
 
 function HomePage() {
     const width = 800;
@@ -18,11 +18,11 @@ function HomePage() {
                 width={width}
                 height={height/2}
             />
-            <ConnectedScatterPlot
+            <QuantileDotPlot
                 width={width}
                 height={height/2}
             />
-            <QuantileDotPlot
+            <LayerdChart
                 width={width}
                 height={height/2}
             />
