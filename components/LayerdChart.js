a5fcd805bb86fdfaf2d56dea3f7337e9464f64ea
motttey <motitago@gmail.com>
2022-08-29 08:55:45 +0900

    modify: use stacked area chart

--- components/LayerdChart.js
+++ components/LayerdChart.js
@@ -12,7 +12,7 @@ function LayerdChart (props) {
         data, width, height
     ) => {
         return {
-            "$schema": "https://vega.github.io/schema/vega-lite/v2.json",
+            "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
             "data": data,
             "width": width,
             "height": height,
@@ -21,16 +21,20 @@ function LayerdChart (props) {
                     "mark": "area",
                     "encoding": {
                         "x": {
-                            "field": "x",
+                            "field": "year",
                             "type": "quantitative",
                         },
                         "y": {
-                            "aggregate": "mean",
+                            "aggregate": "sum",
                             "field": "y",
                             "type": "quantitative",
                             "axis": {
                                 "grid": false
                             }
+                        },
+                        "color": {
+                            "field": "type",
+                            "scale": {"scheme": "category20b"}
                         }
                     }
                 },
@@ -38,7 +42,7 @@ function LayerdChart (props) {
                     "mark": "line",
                     "encoding": {
                         "x": {
-                            "field": "x",
+                            "field": "year",
                             "type": "quantitative",
                         },
                         "y": {
@@ -58,26 +62,32 @@ function LayerdChart (props) {
         };
     };
 
-    const data = {
-        "values": [
-            {"x": 1,  "y": 28}, {"x": 2,  "y": 55},
-            {"x": 3,  "y": 43}, {"x": 4,  "y": 91},
-            {"x": 5,  "y": 81}, {"x": 6,  "y": 53},
-            {"x": 7,  "y": 19}, {"x": 8,  "y": 87},
-            {"x": 9,  "y": 52}, {"x": 10, "y": 48},
-            {"x": 11, "y": 24}, {"x": 12, "y": 49},
-            {"x": 13, "y": 87}, {"x": 14, "y": 66},
-            {"x": 15, "y": 17}, {"x": 16, "y": 27},
-            {"x": 17, "y": 68}, {"x": 18, "y": 16},
-            {"x": 19, "y": 49}, {"x": 20, "y": 15}
-        ]
+    const getData = () => {
+        const data = [];
+        let x = 10;
+        let y = 25;
+        for (let i = 1900; i < 2000; i++) {
+            x = i;
+            ["a", "b"].map((type) => {
+                y = y + 5 * (Math.random() -  0.5);
+                data.push({ 
+                    year: i, 
+                    x: x, 
+                    y: y,
+                    type: type 
+                });
+            })
+        }
+        return {
+            values: data
+        };
     };
 
     return (
         <Chart
             componentName="horizongraph"
             json={getLayerdChart(
-                data,
+                getData(),
                 props.width,
                 props.height
             )}
